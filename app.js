eb62643024eae4c27e125be73aebbb3c202518e9
DanielTndj <daniel.tndj@gmail.com>
2020-11-06 20:45:28 +0700

    Setup mongo connection and created user registration & login

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect(
+  "mongodb://localhost:27017/userDB",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) =>
+    err ? res.send(err) : console.log("Succesfully connected to mongoose")
+);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const UserModel = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -16,6 +38,29 @@ app.get("/register", (req, res) => {
 
 app.get("/login", (req, res) => {
   res.render("login");
+});
+
+app.post("/register", (req, res) => {
+  const user = new UserModel({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  user.save((err) => (err ? console.log(err) : res.render("secrets")));
+});
+
+app.post("/login", (req, res) => {
+  UserModel.findOne({ username: req.body.username }, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (result) {
+        if (result.password === req.body.password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, () =>
