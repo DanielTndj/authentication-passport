918ed9c7a4ecd71ce13211df5861b002d40c709a
DanielTndj <daniel.tndj@gmail.com>
2020-11-13 21:07:38 +0700

    Implement authentication using passport

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-// const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Setting session
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Setting passport
+app.use(passport.initialize());
+// Setting passport untuk mengatur session
+app.use(passport.session());
 
 mongoose.connect(
   "mongodb://localhost:27017/userDB",
@@ -20,19 +34,19 @@ mongoose.connect(
 );
 
 const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: String,
+  password: String,
 });
 
-// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// menggunakan plugin local mongoose yang digunakan untuk hash & salt password
+userSchema.plugin(passportLocalMongoose);
 
 const UserModel = new mongoose.model("User", userSchema);
+
+// digunakan untuk authenticate user dari username dan password
+passport.use(UserModel.createStrategy());
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -42,37 +56,57 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+
 app.get("/login", (req, res) => {
-  res.render("login");
+  if (req.isAuthenticated()) {
+    res.redirect("/secrets");
+  } else {
+    res.render("login");
+  }
 });
 
 app.post("/register", (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const user = new UserModel({
-      username: req.body.username,
-      password: hash,
-    });
-
-    user.save((err) => (err ? console.log(err) : res.render("secrets")));
-  });
+  UserModel.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () =>
+          res.redirect("/secrets")
+        );
+      }
+    }
+  );
 });
 
 app.post("/login", (req, res) => {
-  UserModel.findOne({ username: req.body.username }, (err, result) => {
+  const user = new UserModel({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, (err) => {
     if (err) {
       console.log(err);
     } else {
-      if (result) {
-        bcrypt.compare(req.body.password, result.password, (err, result) => {
-          if (!err) {
-            !result ? console.log("Wrong password") : res.render('secrets')
-          } else {
-            console.log("Can't login");
-          }
-        });
-      }
+      passport.authenticate("local")(req, res, () => res.redirect("/secrets"));
     }
   });
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () =>
